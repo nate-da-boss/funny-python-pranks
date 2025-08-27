d647b723551462572cb81070dbb63eec16c134e6
nate-da-boss <nathanielyt233@gmail.com>
2025-08-27 12:15:55 -0300

    Add files via upload

---
+++ niche-minion-meem.py
@@ -0,0 +1,7 @@
+import webbrowser
+
+url = "https://github.com/nate-da-boss/tuff-meem/blob/main/IMG_2184.png?raw=true"
+webbrowser.open (url)
+
+url2 = "https://github.com/nate-da-boss/tuff-meem/blob/main/IMG_2184.png?raw=true"
+webbrowser.open (url2)
